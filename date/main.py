029ff8069618b066c7c08c618729740abe825957
马赛飞 <1017629235@qq.com>
2022-01-06 12:05:37 +0800

    更新脚本，删除未用到的方法

--- date/main.py
+++ date/main.py
@@ -1,47 +1,8 @@
 import pymysql
 from bs4 import BeautifulSoup
 import requests
-import re
+
 import datetime
-
-def saveData(start):
-    r = requests.get(
-        'https://store.steampowered.com/search/results/?query&start=' + start + '&count=50&dynamic_data=&sort_by=_ASC&snr=1_7_7_gl')
-    soup = BeautifulSoup(r.text, 'lxml')
-    # 获取名字
-    name = soup.find_all(class_='title')
-    for i in range(len(name)):
-        name[i] = name[i].string
-    # 获取封面图
-    img = soup.find_all('img')
-    for i in range(len(img)):
-        img[i] = img[i].get('src')
-    # 获取发布日期
-    releaseDate = soup.find_all(class_='col search_released responsive_secondrow')
-    for i in range(len(releaseDate)):
-        releaseDate[i] = releaseDate[i].string
-    # 获取详情页面
-    detailsPage = re.findall('https://store.steampowered.com/app/.+?"', r.text)
-    for i in range(len(detailsPage)):
-        detailsPage[i] = detailsPage[i][0:len(detailsPage[i]) - 1]
-    price = []
-    # 包含了每个游戏的价格，折扣情况的母标签
-    bigBox = soup.find_all(class_='responsive_search_name_combined')
-    # col.search_price.discounted.responsive_secondrow
-    box = []
-    for i in bigBox:
-        # 无折扣 col search_price_discount_combined responsive_secondrow
-        # 有折扣col search_price_discount_combined responsive_secondrow
-        price.append(i.find(class_='col search_price_discount_combined responsive_secondrow')['data-price-final'])
-    count = len(name);
-    if (len(img) < count or len(price) < count or len(releaseDate) < count or len(detailsPage) < count):
-        print('error data' + str(int(start) / 50))
-        return
-    db = pymysql.connect(host="127.0.0.1", user="root", password="", database="exam", charset="utf8")
-    for i in range(len(name)):
-        writeToDatabase(db, name[i], img[i], price[i], releaseDate[i], detailsPage[i])
-    db.close()
-
 
 def writeToDatabase(db, name, coverImage, price, releaseDate, detailsPage):
     cursor = db.cursor()
