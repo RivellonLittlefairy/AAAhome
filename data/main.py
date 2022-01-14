a9bed89717e2027fea2976d7db6f927f3e5a4188
msf <1017629235@qq.com>
2022-01-14 08:33:08 +0000

    update data/main.py. 修改sql

--- data/main.py
+++ data/main.py
@@ -15,12 +15,12 @@ def writeToDatabase(db, name, coverImage, price, releaseDate, detailsPage):
     # 处理单引号保存
     name = name.replace('\'', '\'\'')
     cursor = db.cursor()
-    sql = 'select price_now from exam01 where game_name=\'' + name + '\''
+    sql = 'select price_now from game_info_steam where game_name=\'' + name + '\''
     cursor.execute(sql)
     prices = cursor.fetchall()
     if len(prices) == 0:
         # 假如没有这个商品的记录，则插入一条记录最后一个字段值是当前时间戳
-        sql = 'INSERT INTO exam01 (game_name, coverimage_url, price_now,price_lowest,relase_date, detail_page,nearly_lowest_timestamp)VALUES('
+        sql = 'INSERT INTO game_info_steam (game_name, coverimage_url, price_now,price_lowest,relase_date, detail_page,nearly_lowest_timestamp)VALUES('
         sql2 = '\'' + name + '\',' + '\'' + coverImage + '\',' + '\'' + price + '\',' + '\'' + price + '\',' + '\'' + releaseDate + '\',' + '\'' + detailsPage + '\',' + '\'' + str(
             time.time()) + '\');'
         try:
@@ -32,16 +32,16 @@ def writeToDatabase(db, name, coverImage, price, releaseDate, detailsPage):
         # 如果数据库中的当前价格大于商城现在的价格，则需要更新库中价格，并且可能现在是史低价格，则更新时间戳
         if int(price) < list(prices[0])[0]:
             try:
-                sql = 'update exam01 set price_now=\'' + price + '\' where game_name=\'' + name + '\''
+                sql = 'update game_info_steam set price_now=\'' + price + '\' where game_name=\'' + name + '\''
                 cursor.execute(sql)
-                sql = 'select price_lowest from exam01 where game_name=\'' + name + '\''
+                sql = 'select price_lowest from game_info_steam where game_name=\'' + name + '\''
                 cursor.execute(sql)
                 lowestPrice = cursor.fetchall()
                 # 如果当前价格是史低
                 if list(lowestPrice[0])[0] >= int(price):
-                    sql = 'update exam01 set price_lowest=\'' + price + '\' where game_name=\'' + name + '\''
+                    sql = 'update game_info_steam set price_lowest=\'' + price + '\' where game_name=\'' + name + '\''
                     cursor.execute(sql)
-                    sql = 'update exam01 set nearly_lowest_timestamp=\'' + str(
+                    sql = 'update game_info_steam set nearly_lowest_timestamp=\'' + str(
                         time.time()) + '\' where game_name=\'' + name + '\''
                     cursor.execute(sql)
                 db.commit()
@@ -50,13 +50,13 @@ def writeToDatabase(db, name, coverImage, price, releaseDate, detailsPage):
         else:
             if int(price) > list(prices[0])[0]:
                 try:
-                    sql = 'update exam01 set price_now=\'' + price + '\' where game_name=\'' + name + '\''
+                    sql = 'update game_info_steam set price_now=\'' + price + '\' where game_name=\'' + name + '\''
                     cursor.execute(sql)
                     db.commit()
                 except:
                     db.rollback()
             else:
-                sql = 'update exam01 set nearly_lowest_timestamp=\'' + str(
+                sql = 'update game_info_steam set nearly_lowest_timestamp=\'' + str(
                     time.time()) + '\' where game_name=\'' + name + '\''
                 cursor.execute(sql)
                 db.commit()
