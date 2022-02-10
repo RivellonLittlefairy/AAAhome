5b2fd2e288ab2f10a05f85295af69025fa7f84fb
msf <1017629235@qq.com>
2022-02-10 08:00:00 +0000

    update data/pretreatment.py. bug

--- data/pretreatment.py
+++ data/pretreatment.py
@@ -1,8 +1,8 @@
-from getDB import getDBConnection
+import pymysql
 
 
 def pretreatment():
-    db = getDBConnection()
+    db = pymysql.connect(host="127.0.0.1", user="root", password="fs4txdya", database="game_price_info", charset="utf8", port=3306)
     cursor = db.cursor()
     cursor.execute("select count(*) from game_info_steam")
     count = cursor.fetchall()
